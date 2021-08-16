9ba4376004ad8e3382afec0b75b683112c592523
choisungwook <kgg1959@naver.com>
2021-08-16 12:39:30 +0900

    feat: 프로젝트 초기화

---
+++ src/main/java/com/demo/jenkinsPullTest/JenkinsPullTestApplication.java
@@ -0,0 +1,13 @@
+package com.demo.jenkinsPullTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPullTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsPullTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/jenkinsPullTest/JenkinsPullTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.jenkinsPullTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPullTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
